3faec9668286df760a1b14ac190cca13e47f40ee
Nishtha <Nishthaverma7860@gmail.com>
2022-05-11 13:16:00 +0530

    fixing

--- app/src/main/java/com/example/cheqclone/introSlide1.kt
+++ app/src/main/java/com/example/cheqclone/introSlide1.kt
@@ -7,10 +7,14 @@ import androidx.viewpager.widget.ViewPager
 class introSlide1 : AppCompatActivity() {
     lateinit var viewPager : ViewPager
     var layouts: List<Int> = listOf(R.layout.slide1,R.layout.slide2,R.layout.slide3,R.layout.slide4)
-
+    lateinit var mpagerAdaptor: mPagerAdaptor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_slide1)
         viewPager = findViewById(R.id.viewPager)
+        mpagerAdaptor = mPagerAdaptor(this,layouts)
+        viewPager.setAdapter(mpagerAdaptor)
+
+
     }
 }
